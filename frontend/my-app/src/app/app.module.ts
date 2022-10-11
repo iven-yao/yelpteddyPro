c9605cd8209ec06dd2e5abc8badc41b90bad7ad0
ivenyao <ivenyao820109@gmail.com>
2022-10-11 02:55:13 -0700

    front-end add top-nav component and basic routing

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { TestComponent } from './test/test.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { BookingComponent } from './booking/booking.component';
+
+const appRoutes: Routes = [
+  { path: 'search', component:SearchComponent, pathMatch:'full'},
+  { path: 'booking', component: BookingComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    TestComponent,
+    SearchComponent,
+    BookingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
